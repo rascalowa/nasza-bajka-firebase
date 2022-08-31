23741c4c441a503af171a9ffb4ce5a71c10a2a3e
SOFTSERVE\msiko2 <msiko2@softserveinc.com>
2022-08-31 21:14:22 +0200

    Components added but not linked

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,11 @@ import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { PostListComponent } from './posts/post-list/post-list.component';
 
 const routes: Routes = [
-  { path: '' , component: PostListComponent },
+  // { path: '' , component: HomeComponent },
+  { path: '' , component: PostListComponent }, // TO BE CHANGED
+  { path: 'mieszkancy' , component: PostListComponent },
+  { path: 'oferta' , component: PostListComponent },
+  { path: 'kontakt' , component: PostListComponent },
   { path: 'szefowa' , component: PostCreateComponent },
   { path: 'edit/:postId' , component: PostCreateComponent },
   { path: '*' , component: PostListComponent },
---
+++ src/app/mieszkancy/mieszkancy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mieszkancy',
+  templateUrl: './mieszkancy.component.html',
+  styleUrls: ['./mieszkancy.component.css']
+})
+export class MieszkancyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
