115d0d38f1c2f63458ee3cf70f8e7dfa6ed6ce15
SOFTSERVE\msiko2 <msiko2@softserveinc.com>
2023-01-17 19:34:27 +0100

    facebook link added

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -3,6 +3,9 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { DBService } from '../service/db.service';
 import { ContactService } from './contact.service';
 import { LoaderService } from '../service/loader.service';
+import { LAYOUT_ENUM } from '../constans/layout.constans';
+import { Observable, Subject, takeUntil, tap } from 'rxjs';
+import { LayoutService } from '../service/layout.service';
 
 @Component({
   selector: 'app-contact',
@@ -13,6 +16,9 @@ export class ContactComponent implements OnInit {
   mainPhoto: string;
   smallPhoto: string;
   messageSended = false;
+  screenSize$: Observable<LAYOUT_ENUM>;
+  componentDestroyed$ = new Subject<void>();
+  isMobile: boolean;
 
   contactForm = new FormGroup({
     name: new FormControl(''),
@@ -24,8 +30,11 @@ export class ContactComponent implements OnInit {
   constructor(
     private contactService: ContactService,
     private dbService: DBService,
+    private layoutService: LayoutService,
     private readonly loaderService: LoaderService
-    ) {}
+    ) {
+      this.screenSize$ = this.layoutService.size$.asObservable();
+    }
 
   ngOnInit() {
     this.loaderService.setLoading(true);
@@ -37,6 +46,16 @@ export class ContactComponent implements OnInit {
     .catch((error) => {
       console.log(error.message);
     });
+
+    this.screenSize$
+      .pipe(
+        takeUntil(this.componentDestroyed$),
+        tap(data => {
+          console.log(data)
+          this.isMobile = data === LAYOUT_ENUM.XSMALL || data === LAYOUT_ENUM.SMALL;
+        })
+      )
+      .subscribe()
   }
 
   onSubmit(form) {
@@ -54,6 +73,7 @@ export class ContactComponent implements OnInit {
 
   onDestroy() {
     this.messageSended = false;
+    this.componentDestroyed$.next();
   }
 
   resetForm() {
